ebd69c5cfd84347e3f33747cccb499b50f05f6da
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-11-12 15:43:12 -0500

    can logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
 
-const Logout = () => {        
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const Logout = () => {  
+    const {push} = useHistory();
+
+    useEffect(()=> {
+        const token = localStorage.getItem("token");
+
+        axiosWithAuth()
+            .post('http://localhost:5000/api/logout')
+            .then(resp => {
+                localStorage.removeItem('token');
+                push('/login');
+            });
+    }, []);
+      
+    
     return(<div></div>);
 }
 
